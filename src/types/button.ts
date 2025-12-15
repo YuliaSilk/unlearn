3f07294d6b5457e310f0493587e82a1cc6867cbe
YuliaSilk <yuliashevchyk0506@gmail.com>
2025-12-15 23:54:35 +0200

    claen unnesessory code

--- src/types/button.ts
+++ src/types/button.ts
@@ -1,10 +1,3 @@
-// import type { ButtonHTMLAttributes, ReactNode } from "react";
-
-// export interface ButtonProps
-//   extends ButtonHTMLAttributes<HTMLButtonElement> {
-//   children: ReactNode;
-// }
-
 import type { ButtonHTMLAttributes, ReactNode } from "react";
 
 export interface ButtonProps extends Omit<ButtonHTMLAttributes<HTMLButtonElement>, 'children'> {
