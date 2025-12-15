528de50d4854f5be4f8913911abfe6f53757e72a
YuliaSilk <yuliashevchyk0506@gmail.com>
2025-12-15 08:53:30 +0200

    add button

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import "./index.css";
 import Header from "./components/layout/Header/Header";
+import Hero from "./components/Hero";
 
 function App() {
  return (
   <div className="min-h-screen overflow-x-hidden">
    <Header />
-   <main>{/* <Hero /> */}</main>
+   <main>
+    <Hero />
+   </main>
   </div>
  );
 }
---
+++ src/components/Hero.tsx
@@ -0,0 +1,18 @@
+export default function Hero() {
+ return (
+  <section className="relative flex min-h-screen items-center justify-center pt-20">
+   <div className="text-center">
+    <div className="overflow-hidden">
+     <h1 className="text-[84px] font-medium  tracking-tight">
+      Building the future <br />
+      of medicine with AI
+     </h1>
+    </div>
+
+    <div className="mt-4 overflow-hidden">
+     <p className="text-lg text-white/70">Digital experiences that inspire</p>
+    </div>
+   </div>
+  </section>
+ );
+}
